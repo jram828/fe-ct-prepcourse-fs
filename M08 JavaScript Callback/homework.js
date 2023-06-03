29aed88efb51509cb1fe1c9c3d4c8b6211a15d87
jram828 <128335805+jram828@users.noreply.github.com>
2023-06-03 14:29:13 -0500

    Modulo 8 completo

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,12 +5,18 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   let nombresep=nombre.split('');
+   let May=nombresep[0].toUpperCase()
+   nombresep[0]=May
+   let nombremay=nombresep.join('')
+   return nombremay
 }
-
+console.log(mayuscula("mario"))
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb()
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -18,6 +24,7 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb(num1,num2);
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -26,6 +33,9 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   var suma=0;
+   let Total=arrayOfNumbers.reduce((suma,num)=>(suma+num))
+   cb(Total)
 }
 
 function forEach(array, cb) {
@@ -33,6 +43,9 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   let calleach=array.forEach((elemento)=>(cb(elemento)));
+   return calleach;
+
 }
 
 function map(array, cb) {
@@ -40,6 +53,8 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   let callmap=array.map((elemento)=>(cb(elemento)));
+   return callmap;
 }
 
 function filter(arrayOfStrings) {
